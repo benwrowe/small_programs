ea2bbeb52ac7dd1551fbe264e1870c5123732c13
Ben Rowe <mrbwrowe@gmail.com>
2023-08-21 19:57:05 +0100

    second

--- angry_boss.rb
+++ angry_boss.rb
@@ -4,3 +4,7 @@ puts "A raise or a company helicopter?"
 wants_and_desires = gets.chomp
 system "sleep 1"
 puts "You are out of your mind? #{wants_and_desires.upcase}?!? YOU'RE FIRED!!!"
+
+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
+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
+
