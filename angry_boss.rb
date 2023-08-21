81614be6d792e4a09e781d4c8e5221fa7c4cd3ef
Ben Rowe <mrbwrowe@gmail.com>
2023-08-21 18:48:27 +0100

    first files

---
+++ angry_boss.rb
@@ -0,0 +1,6 @@
+puts "I'm your Boss!"
+puts "What you want?"
+puts "A raise or a company helicopter?"
+wants_and_desires = gets.chomp
+system "sleep 1"
+puts "You are out of your mind? #{wants_and_desires.upcase}?!? YOU'RE FIRED!!!"
